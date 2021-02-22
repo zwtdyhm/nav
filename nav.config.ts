dbe1187c1b39e04da726a9e6c5a7837ee56f4d0e
张文涛 <zwtdyhm@hotmail.com>
2021-02-22 10:01:24 +0800

    Update nav.config.ts

--- nav.config.ts
+++ nav.config.ts
@@ -2,7 +2,7 @@ import { IConfig } from './src/types'
 
 const c: IConfig = {
   // [必填], 请填写您的仓库地址
-  gitRepoUrl: 'https://github.com/xjh22222228/nav',
+  gitRepoUrl: 'https://github.com/zwtdyhm/nav',
 
   // 部署分支
   branch: 'main',
@@ -14,13 +14,13 @@ const c: IConfig = {
   showGithub: true,
 
   // 您的网站地址，有利于SEO
-  homeUrl: 'https://nav3.cn',
+  homeUrl: 'https://zwtdyhm.github.io/nav',
 
   // 网站标题
-  title: '发现导航 - 精选实用导航网站',
+  title: '导航 - 精选实用导航网站',
 
   // 网站描述
-  description: '发现导航 - 精选实用导航网站',
+  description: '导航 - 精选实用导航网站',
 
   // 网站关键字
   keywords: '导航,前端资源,社区站点,设计师,实用工具,学习资源,运营,网络安全,node.js',
@@ -31,12 +31,12 @@ const c: IConfig = {
   // 网站底部内容, 版权信息、备案号, 可以是 HTML
   footerContent: `
     <div style="font-weight: bold;">共收录\${total}个网站</div>
-    <div>Copyright © 2018-2021 nav3.cn, All Rights Reserved</div>
+    <div>Copyright © 2018-2021 , All Rights Reserved</div>
   `,
 
   // 百度统计地址
   // https://tongji.baidu.com/web/welcome/login
-  baiduStatisticsUrl: 'https://hm.baidu.com/hm.js?4582be7af7e7c95ef75351e07c6c32ba',
+  baiduStatisticsUrl: '',
 
   // CNZZ 统计地址
   // https://www.cnzz.com/o_index.php
@@ -44,7 +44,7 @@ const c: IConfig = {
 
   // Light 主题配置
   lightThemeConfig: {
-    // https://www.nav3.cn/#/light?q=grabient
+    // https://zwtdyhm.github.io/nav/#/light?q=grabient
     backgroundLinear: [
       'linear-gradient(62deg, #8EC5FC 0%, #E0C3FC 100%)',
       'linear-gradient(90deg, #FEE140 0%, #FA709A 100%)',
@@ -79,7 +79,7 @@ const c: IConfig = {
   simThemeConfig: {
     // 海报图
     posterImageUrls: [
-      'https://raw.sevencdn.com/xjh22222228/nav/image/sim-wallpaper.jpg'
+      'https://raw.sevencdn.com/zwtdyhm/nav/image/sim-wallpaper.jpg'
     ],
     description: '这里收录多达 <b>${total}</b> 个优质网站， 助您工作、学习和生活'
   }
